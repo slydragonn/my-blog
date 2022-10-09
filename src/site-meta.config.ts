3e41cf0ec33cb666ac2807116c9a5735bd6c8e77
slydragonn <alogo18m@gmail.com>
2022-10-08 20:30:48 -0500

    initial commit

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
+  title: "Alejandro Londoño",
+  description: "Hi, I’m a Front-End Developer and this is my blog, where I write about programming, tools, and other exciting topics.",
   lang: "en-GB",
   ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  githubUrl: "https://github.com/slydragonn",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
