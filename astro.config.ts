90ccabb39866334be0984a41501740d8c6ae2fee
slydragonn <alogo18m@gmail.com>
2022-10-10 15:32:30 -0500

    feat: add vercel config

--- astro.config.ts
+++ astro.config.ts
@@ -3,21 +3,22 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	markdown: {
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	site: "",
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: { applyBaseStyles: false },
-		}),
-		image(),
-		sitemap(),
-	],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true
+    }
+  },
+  site: "",
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
